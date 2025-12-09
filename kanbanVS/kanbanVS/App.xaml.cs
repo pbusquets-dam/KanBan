58fb66d819cb0abcb1ad56a2e5f7c66000350c73
Polete <pbusquets-dam@pbusquets.dam@institutcampalans.net>
2025-12-09 18:31:30 +0100

    login

--- kanbanVS/kanbanVS/App.xaml.cs
+++ kanbanVS/kanbanVS/App.xaml.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace kanbanVS
 {
-    /// <summary>
-    /// Lógica de interacción para App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            LoginWindow login = new LoginWindow();
+            bool? result = login.ShowDialog();
+
+            if (result == true)
+            {
+                MainWindow main = new MainWindow();
+                Application.Current.MainWindow = main;
+                main.Show();
+            }
+            else
+            {
+                Shutdown();
+            }
+        }
     }
 }
---
+++ kanbanVS/kanbanVS/LoginWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace kanbanVS
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            string user = UserTextBox.Text;
+            string pass = PassBox.Password;
+
+            if (user == "admin" && pass == "admin")
+            {
+                this.DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Usuari o contrasenya incorrectes", "Error",
+                                MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
